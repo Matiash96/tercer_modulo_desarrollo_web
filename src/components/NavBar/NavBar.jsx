4bd4cfe7cb8e1dc6fffba3c20fdbc63cc20f331a
Matias Marchetti <mhmarchetti96@gmail.com>
2023-11-12 20:51:47 -0300

    Se trabaja en el footer de la página

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,13 +7,14 @@ export const NavBar = () => {
         <div className='nav'>
             <div className='nav-logo'>
                 <Link className='link' to="/">
-                    <img className='imagen' src="/media/logo_tienda.webp" alt="" />
+                    <img className='imagen' src="/media/logo_tienda.webp" alt="Pesca-Mar" />
                 </Link>
                 <Link className='link nombre-tienda' to="/">Pesca-Mar</Link>
             </div>
             <div className='div-nav-link'>
                 <Link className='link nav-link' to="/canias">Cañas</Link>
                 <Link className='link nav-link' to="/reeles">Reeles</Link>
+                <img className='imagenCarrito' src="/media/carrito.png" alt="" />
             </div>
         </div>
     )
